be42a7d974285a8491b766b111973c1ad240c168
root <root@s222637.hostiman.com>
2022-07-27 14:20:54 +0300

    исправления в пускачах

--- dockers/phpmyadmin.cross.bat
+++ dockers/phpmyadmin.cross.bat
@@ -1,3 +1,1 @@
-docker stop apv-phpmyadmin
-docker rm apv-phpmyadmin
-docker run -it -d -e MYSQL_ROOT_PASSWORD=apvMysqlPassword -e PMA_HOST=apv-db --link apv-db -p 8080:80 --name apv-phpmyadmin phpmyadmin
+docker run --restart=always -d -e MYSQL_ROOT_PASSWORD=apvMysqlPassword -e PMA_HOST=apv-db --link apv-db -p 8080:80 --name apv-phpmyadmin phpmyadmin
