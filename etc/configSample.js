9c1c1bb01d9f5eb593df0a79f97b087bb78a8680
root <root@s222638.hostiman.com>
2023-02-14 15:09:25 +0300

    configSample

--- etc/configSample.js
+++ etc/configSample.js
@@ -1,11 +1,19 @@
 const config = {
   local_port: "3000",
-  base_url: "http://127.0.0.1:3000/",
-  db_host: "81.90.180.23",
-  db_user: "nkd-user",
-  db_password: "sdfvsfdv",
-  db_name: "nkd",
+  db_host: "",
+  db_user: "apvuser",
+  db_password: "",
+  db_name: "apv",
   db_prefix: "pattern",
+  configTTL: 120,
+  botToken: "5478135924:AAEIN8IIsdhaauOubTcdUMD2SUVT_wc3Ly0",
+  sync_TTL: 3600,
+  sync_CheckPeriod: 60,
+  sync_LinkDevice: 1000,
+  sync_offlineCode: 1001,
+  sync_onlineCode: 1002,
+  offlineReminderSheduler: "0 8 * * *",
+  daylyStatisticSheduler: "0 1 * * *",
 };
 
 module.exports = config;
